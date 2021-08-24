cdbd2381ff0c097ca067f4d15314e763b9d8b9bc
WowohowoW <4edwinn@gmail.com>
2021-08-23 23:55:27 -0700

    Connected layers have been added , installed the missing navigation/stack; test ran to confirm compile

--- App.js
+++ App.js
@@ -3,18 +3,18 @@ import React from 'react';
 
 
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LandingScreen from './components/auth/landing'
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
-  <NavigationContainer>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName= "landing">
+              <Stack.Screen name = "landing" component ={LandingScreen}  options ={{headerShown: false}} />
+          </Stack.Navigator>
 
-Hasdfasdfasdf
+        </NavigationContainer>
 
-  </NavigationContainer>
-
-  );
+  )
 }
-
-
-});
